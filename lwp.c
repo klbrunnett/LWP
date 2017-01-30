cc89e15570db6f10e2d319bcc327a51e66858701
Kyle Brunnett <klbrunnett@gmail.com>
2017-01-29 20:56:11 -0800

    add lwp functions

---
+++ lwp.c
@@ -0,0 +1,81 @@
+/*
+ * Creates a new lightweight process which executes the given function
+ * with the given argument. The new processes’s stack will be
+ * stacksize words.
+ * lwp_create() returns the (lightweight) thread id of the new thread
+ * ir −1 if the thread cannot be created.
+ */
+tid_t lwp_create(function,argument,stacksize) {
+
+}
+
+/*
+ * Returns the tid of the calling LWP or NO THREAD if not called by a
+ * LWP.
+ */
+void lwp_gettid() {
+
+}
+
+/*
+ * Yields control to another LWP. Which one depends on the scheduler.
+ * Saves the current LWP’s context, picks the next one, restores
+ * that thread’s context, and returns.
+ */
+tid_t lwp_exit() {
+
+}
+
+/*
+ * Terminates the current LWP and frees its resources. Calls
+ * sched->next() to get the next thread. If there are no other
+ * threads, restores the original system thread.
+ */
+void lwp_yield() {
+
+}
+
+/* 
+ * Starts the LWP system. Saves the original context (for lwp stop()
+ * to use later), picks a LWP and starts it running. If there are no
+ * LWPs, returns immediately.
+ */
+void lwp_start() {
+
+}
+
+/*
+ * stop the LWP systemStops the LWP system, restores the original stack pointer and returns
+ * to that context. (Wherever lwp start() was called from.
+ * lwp stop() does not destroy any existing contexts, and thread
+ * processing will be restarted by a call to lwp start().
+ */
+void lwp_stop() {
+
+}
+
+/*
+ * Causes the LWP package to use the given scheduler to choose the
+ * next process to run. Transfers all threads from the old scheduler to
+ * the new one in next() order. If scheduler is NULL, or has never
+ * been set, the scheduler should do round-robin scheduling.
+ */
+void lwp_set_scheduler(scheduler) {
+
+}
+
+/*
+ * Returns the pointer to the current scheduler.
+ */
+scheduler lwp_get_scheduler(void) {
+
+}
+
+/*
+ * Returns the thread corresponding to the given thread ID, or NULL
+ * if the ID is invalid
+ */
+
+thread tid2thread(tid) {
+
+}
